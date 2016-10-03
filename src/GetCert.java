821c529e2946c572b232a2d9c75d024a67efc174
teadur <georg@gj.ee>
2016-10-03 20:35:38 +0300

    disable java certficate validation

--- src/GetCert.java
+++ src/GetCert.java
@@ -1,19 +1,56 @@
 /**
  * Created by georg on 3.10.2016.
  */
-import java.io.IOException;
 import java.net.URL;
-import java.security.cert.Certificate;
-
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+
 
 public class GetCert {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+    /*
+     *  fix for
+     *    Exception in thread "main" javax.net.ssl.SSLHandshakeException:
+     *       sun.security.validator.ValidatorException:
+     *           PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException:
+     *               unable to find valid certification path to requested target
+     */
+        TrustManager[] trustAllCerts = new TrustManager[] {
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
 
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {  }
 
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {  }
 
-        /* URL httpsURL = new URL("https://www.google.com.br/"); */
-         URL httpsURL = new URL("https://registrar.internet.ee/");
+                }
+        };
+
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+    /*
+     * end of the fix
+     */
+
+        URL httpsURL = new URL("https://registrar.internet.ee/");
 
         HttpsURLConnection connection = (HttpsURLConnection) httpsURL.openConnection();
         connection.connect();
