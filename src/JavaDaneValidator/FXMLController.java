0f8c5272143f98045f990173db56c229c64074be
teadur <georg@gj.ee>
2017-01-03 18:52:27 +0200

    add exit button

--- src/JavaDaneValidator/FXMLController.java
+++ src/JavaDaneValidator/FXMLController.java
@@ -3,6 +3,7 @@ package JavaDaneValidator;
 /**
  * Created by georg on 3.01.2017.
  */
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
@@ -14,5 +15,8 @@ public class FXMLController {
     @FXML protected void handleSubmitButtonAction(ActionEvent event) {
         actiontarget.setText("Hello world action");
     }
-
+    @FXML protected void handleSubmitButtonExit(ActionEvent event) {
+        Platform.exit();
+        System.exit(0);
+    }
 }
