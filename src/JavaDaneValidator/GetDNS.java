51d33fc3c2af9e67375baccec55b927ca1783cc1
teadur <georg@gj.ee>
2017-01-03 22:09:05 +0200

    Cleanup GetDNS

--- src/JavaDaneValidator/GetDNS.java
+++ src/JavaDaneValidator/GetDNS.java
@@ -1,7 +1,6 @@
 package JavaDaneValidator;
 
 import org.xbill.DNS.*;
-import java.util.Iterator;
 
 /**
  * Created by georg on 3.01.2017.
@@ -9,50 +8,13 @@ import java.util.Iterator;
 public class GetDNS {
 
 
-    public static void main(String validateurl) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-/**
- Default inet resolver example
- InetAddress addr = Address.getByName("data.internet.ee");
- System.out.print(addr);
-
- **/
-
-        /**
-         * GET MX record example
-         Record [] records = new Lookup("gmail.com", Type.MX).run();
-         for (int i = 0; i < records.length; i++) {
-         MXRecord mx = (MXRecord) records[i];
-         System.out.println("Host " + mx.getTarget() + " has preference " + mx.getPriority());
-
-         }
-         **/
-
-        /**
-         GET TLSA Record example
-         Record [] records = new Lookup("_443._tcp.data.internet.ee", Type.TLSA).run();
-         for (int i = 0; i < records.length; i++) {
-
-         TLSARecord tlsaRecord = (TLSARecord) records[i];
-         System.out.println("Host ");
-
-         } **/
-
-        String ipAddress = "2.0.0.127";
-        String dnsblDomain = "sbl-xbl.spamhaus.org";
-        /** String validateurl = "data.internet.ee"; **/
-
-        /** Lookup lookup = new Lookup(ipAddress + "." + dnsblDomain, Type.ANY); **/
-
-        /**
-         *  Because we handle only https url lets hardcode/append _443._tcp." to validateurl
-         */
+        String validateurl = "data.internet.ee";
         Lookup lookup = new Lookup("_443._tcp." + validateurl, Type.ANY);
 
-        /** Lookup lookup = new Lookup("internet.ee", Type.ANY); **/
-
 /**
- * for some reason using system resolver doesnot work, lets hardcode google
+ * for some reason using system resolver doesnot work, lets hardcode google TODO: seadistatav kastist vaikimisi google
  */
         Resolver resolver = new SimpleResolver("8.8.8.8");
         lookup.setResolver(resolver);
@@ -65,17 +27,6 @@ public class GetDNS {
             String listingType = null;
             for (int i = 0; i < records.length; i++)
             {
-                /** we dont actualy care about TEXTRecords remove me later **/
-                if(records[i] instanceof TXTRecord)
-                {
-                    TXTRecord txt = (TXTRecord) records[i];
-                    for(Iterator j = txt.getStrings().iterator(); j.hasNext();)
-                    {
-                        responseMessage += (String)j.next();
-                    }
-                }
-
-                /** lets handle TLSA records **/
 
                 if(records[i] instanceof TLSARecord)
                 {
@@ -92,11 +43,6 @@ public class GetDNS {
                     String[] kogustring = tlsaRecord.toString().split(" ");
 
                     listingType = kogustring[3];
-                    /** for(Iterator j = tlsaRecord.getName(); j.hasNext();)
-                     {
-                     responseMessage += (String)j.next();
-                     } **/
-
                 }
 
                 else if(records[i] instanceof ARecord)
@@ -118,10 +64,6 @@ public class GetDNS {
             System.err.println(lookup.getResult());
             System.err.println("Error!");
         }
-
-
-
-
 
     }
 
